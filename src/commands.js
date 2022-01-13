dc6a7a116e6e94a41377bf32722f02972880eaee
Diego Fernando Alayon <dfar.41@gmail.com>
2022-01-13 14:39:20 -0500

    usando inquirer para recibir argumentos

--- src/commands.js
+++ src/commands.js
@@ -1,10 +1,24 @@
 // const {program} = require('commander')
 import { program } from 'commander'
+import  pkg  from 'inquirer'
+const {prompt} = pkg
 
 program.version('0.0.1')
   .description('A command line tool for managing tasks')
 
-program.command('save <title> <description>').action((title, description) => {
-  console.log(title, description)
+program.command('save').action(async () => {
+  const answers = await prompt([
+    {
+      type: 'input',
+      message: 'Task title',
+      name: 'title'
+    },
+    {
+      type: 'input',
+      message: "Task description",
+      name: "description",
+    }
+  ])
+  console.log(answers)
 })
 program.parse(process.argv) // nos permite recibir los argumentos pasados por consola
