ddfcfb3fd1417461fdeaa8d715a501126fd74a02
Diego Fernando Alayon <dfar.41@gmail.com>
2022-01-13 13:56:37 -0500

    recibiendo argumentos con commander

--- src/commands.js
+++ src/commands.js
@@ -4,7 +4,7 @@ import { program } from 'commander'
 program.version('0.0.1')
   .description('A command line tool for managing tasks')
 
-program.command('save').action(() => {
-  console.log('saving')
+program.command('save <title> <description>').action((title, description) => {
+  console.log(title, description)
 })
 program.parse(process.argv) // nos permite recibir los argumentos pasados por consola
