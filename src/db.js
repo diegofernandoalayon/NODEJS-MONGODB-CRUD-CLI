7f1bb3091c5ce2d6ac36c214b544873dfd7d86ce
Diego Fernando Alayon <dfar.41@gmail.com>
2022-01-13 16:07:40 -0500

    creando index para iniciar la ejecucion desde un solo punto

--- src/db.js
+++ src/db.js
@@ -1,14 +1,15 @@
 import mongoose  from "mongoose"
-
-import 'dotenv/config'
-
 const {MONGO_DB_URI} = process.env
-mongoose.connect(MONGO_DB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => {
-  console.log('connected to MongoDB')
-}).catch((error) => {
-  console.error('error connection to MongoDB', error.message)
-})
+
+export const connectDB = async () => {
+  
+  await mongoose.connect(MONGO_DB_URI, {
+     useNewUrlParser: true,
+     useUnifiedTopology: true
+   }).catch((error) => {
+     console.error('connect error', error.message)
+   })
+  
+ 
+}
+// export {connectDB}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import 'dotenv/config'
+import {connectDB} from './db.js'
+// import './commands.js'
+
+async function main () {
+await connectDB()
+
+}
+main()
