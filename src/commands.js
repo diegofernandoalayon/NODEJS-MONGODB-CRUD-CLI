4ea3dee8589d1505bc9d4b8bc7238d5f0aef5eb1
Diego Fernando Alayon <dfar.41@gmail.com>
2022-01-13 13:53:39 -0500

    creando comandos personalizados con commander

--- src/commands.js
+++ src/commands.js
@@ -4,4 +4,7 @@ import { program } from 'commander'
 program.version('0.0.1')
   .description('A command line tool for managing tasks')
 
+program.command('save').action(() => {
+  console.log('saving')
+})
 program.parse(process.argv) // nos permite recibir los argumentos pasados por consola
