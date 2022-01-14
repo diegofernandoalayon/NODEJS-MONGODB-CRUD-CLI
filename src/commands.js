bc255c161a3078780760c1124eb45ed1f69e9164
Diego Fernando Alayon <dfar.41@gmail.com>
2022-01-14 14:49:36 -0500

    creando comando para listar tareas, y creando funcionalidad para listar tareas

--- src/commands.js
+++ src/commands.js
@@ -1,7 +1,7 @@
 // const {program} = require('commander')
 import { program } from 'commander'
 import inquirer from 'inquirer'
-import {addTask} from './controllers/taks.controllers.js'
+import {addTask, listTask} from './controllers/taks.controllers.js'
 
 program.version('0.0.1')
   .description('A command line tool for managing tasks')
@@ -26,6 +26,8 @@ program.command('save').action(async () => {
     // }
   ])
   addTask(answers)
-  
+})
+program.command('list').action(async () => {
+  listTask()
 })
 program.parse(process.argv) // nos permite recibir los argumentos pasados por consola
--- src/controllers/taks.controllers.js
+++ src/controllers/taks.controllers.js
@@ -1,8 +1,20 @@
 import Task from '../models/Task.js' 
 import {connection} from '../db.js'
+
 export const addTask = async (task) => {
   
-  console.log(task)
   await Task.create(task)
+  console.log('New Task Created')
   await connection.close()
+}
+
+export const listTask = async () => {
+ const tasks =  await Task.find().lean()
+ console.table(tasks.map(task => ({
+   _id: task._id.toString(),
+   title: task.title,
+   description: task.description
+ })))
+ await connection.close()
+ process.exit(0) //para indicar a node que el programa acabo bien, y que no mantenga a la espera 
 }
