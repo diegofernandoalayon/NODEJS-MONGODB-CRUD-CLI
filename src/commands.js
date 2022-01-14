e29842a04e0996ebf2835f79e23cf79d0ea5bd16
Diego Fernando Alayon <dfar.41@gmail.com>
2022-01-14 15:14:02 -0500

    refactorizando save

--- src/commands.js
+++ src/commands.js
@@ -5,20 +5,20 @@ import {addTask, listTask, deleteTask} from './controllers/taks.controllers.js'
 
 program.version('0.0.1')
   .description('A command line tool for managing tasks')
-
+const taksQuestions = [
+  {
+    type: 'input',
+    message: 'Task title',
+    name: 'title'
+  },
+  {
+    type: 'input',
+    message: "Task description",
+    name: "description",
+  }
+]
 program.command('save').alias('s').action(async () => {
-  const answers = await inquirer.prompt([
-    {
-      type: 'input',
-      message: 'Task title',
-      name: 'title'
-    },
-    {
-      type: 'input',
-      message: "Task description",
-      name: "description",
-    }
-  ])
+  const answers = await inquirer.prompt(taksQuestions)
   addTask(answers)
 })
 program.command('list').alias('l').action(() => {
